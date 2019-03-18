9e300b2b9787328bab429b9e41c29b5d47b8e932
Fabio Henrique Sampaio Pedrosa <fpedrosa@laptec.com.br>
2019-03-18 13:05:33 -0300

    validacoao api

--- src/main/java/br/com/pedrosa/api/exception/GlobalExceptionHandler.java
+++ src/main/java/br/com/pedrosa/api/exception/GlobalExceptionHandler.java
@@ -1,15 +1,29 @@
 package br.com.pedrosa.api.exception;
 
 import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.context.request.WebRequest;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
+	
+	@ResponseStatus(HttpStatus.BAD_REQUEST)
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public List<String> handleValidationExceptions(MethodArgumentNotValidException ex) {
+	    return ex.getBindingResult()
+	        .getAllErrors().stream()
+	        .map(ObjectError::getDefaultMessage)
+	        .collect(Collectors.toList());
+	}
 	
 	@ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity<?> resourceNotFoundException(ResourceNotFoundException ex, WebRequest request) {
